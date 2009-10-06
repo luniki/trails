6d800e157d7fc943bc7b5973e656d3fdc5541cb7
Marcus Lunzenauer <mlunzena@uos.de>
2009-10-06 13:55:31 +0200

    add tests for server relative and absolute urls

--- test/lib/controller_render_test.php
+++ test/lib/controller_render_test.php
@@ -20,12 +20,141 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+class FooController extends Trails_Controller {
+  function index_action() {
+  }
 
-class RenderTestCase extends UnitTestCase {
+  function redirect_me_action() {
+    $this->redirect('where');
+  }
+
+  function double_render_action() {
+    $this->render_nothing();
+    $this->render_nothing();
+  }
+
+  function double_render_and_redirect_action() {
+    $this->render_nothing();
+    $this->redirect('where');
+  }
+}
+
+class ControllerRenderTestCase extends UnitTestCase {
   function setUp() {
+    $this->setUpFS();
   }
 
   function tearDown() {
+    stream_wrapper_unregister("var");
+  }
+
+  function setUpFS() {
+    ArrayFileStream::set_filesystem(array(
+      'app' => array(
+        'views' => array(
+          'layout.php' => '[<?= $content_for_layout ?>]',
+          'foo' => array(
+            'index.php'  => 'foo/index',
+          )
+        )
+      ),
+    ));
+    stream_wrapper_register("var", "ArrayFileStream") or die("Failed to register protocol");
+  }
+
+  function test_should_render_default_template() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new FooController($dispatcher);
+    $response = $controller->perform('index');
+    $this->assertEqual($response, new Trails_Response('foo/index'));
+  }
+
+  function test_should_render_index_action_when_there_is_no_other() {
+    $controller = new FilteringController();
+    $controller->expectOnce('before_filter', array('index', array()));
+    $controller->setReturnValue('before_filter', FALSE);
+    $response = $controller->perform('');
+  }
+
+  function test_should_throw_exception_if_action_is_missing() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new FooController($dispatcher);
+    $this->expectException('Trails_UnknownAction');
+    $response = $controller->perform('missing');
+  }
+
+  function test_should_redirect_if_requested() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new Trails_Controller($dispatcher);
+    $controller->redirect('where');
+    $this->assertEqual($controller->get_response(),
+                       new Trails_Response('',
+                                           array('Location' => 'trails_uri/where'),
+                                           302));
+  }
+
+  function test_should_redirect_server_relative_paths() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new Trails_Controller($dispatcher);
+    $controller->redirect('/where');
+    $this->assertEqual($controller->get_response(),
+                       new Trails_Response('',
+                                           array('Location' => '/where'),
+                                           302));
+  }
+
+  function test_should_redirect_to_absolute_urls() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new Trails_Controller($dispatcher);
+    $controller->redirect('http://example.com');
+    $this->assertEqual($controller->get_response(),
+                       new Trails_Response('',
+                                           array('Location' => 'http://example.com'),
+                                           302));
+  }
+
+  function test_should_throw_exception_if_rendering_more_than_once() {
+    $controller = new Trails_Controller(NULL);
+    $this->expectException('Trails_DoubleRenderError');
+    $controller->render_nothing();
+    $controller->render_nothing();
+  }
+
+  function test_should_throw_exception_if_rendering_and_redirecting() {
+    $controller = new Trails_Controller(NULL);
+    $this->expectException('Trails_DoubleRenderError');
+    $controller->render_nothing();
+    $controller->redirect('');
+  }
+
+  function test_should_render_template_undecorated_with_implicit_layout() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new Trails_Controller($dispatcher);
+    $controller->set_layout('layout');
+    $controller->render_template('foo/index');
+    $this->assertEqual($controller->get_response(), new Trails_Response('foo/index'));
+  }
+
+  function test_should_render_template_decorated_with_explicit_layout() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new Trails_Controller($dispatcher);
+    $controller->render_template('foo/index', 'layout');
+    $this->assertEqual($controller->get_response(), new Trails_Response('[foo/index]'));
+  }
+
+  function test_should_render_action() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new FooController($dispatcher);
+    $controller->render_action('index');
+    $this->assertEqual($controller->get_response(), new Trails_Response('foo/index'));
+  }
+
+  function test_should_render_action_with_layout() {
+    $dispatcher = new Trails_Dispatcher('var://app', 'trails_uri', 'default');
+    $controller = new FooController($dispatcher);
+    $controller->set_layout('layout');
+    $controller->render_action('index');
+    $this->assertEqual($controller->get_response(), new Trails_Response('[foo/index]'));
   }
 }
 
